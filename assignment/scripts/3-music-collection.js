9d23f71d25052288eaaea9ac05801dbea25a754c
Cory Anderson <cory.anderson6@yahoo.com>
2023-02-10 00:34:29 -0600

    added showCollection function

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -18,3 +18,13 @@ console.log(addToCollection("Illmatic", "Nas" , 1994));
 console.log(addToCollection("Master of Puppets", "Metallica" , 1986));
 console.log(addToCollection("It's Dark and Hell is Hot", "DMX", 1998));
 console.log(addToCollection("Flesh of My Flesh, Blood of My Blood", "DMX", 1998));
+
+function showCollection(albums) {
+    console.log(albums.length + " albums in the collection");
+
+    for (let i = 0; i < albums.length; i++) {
+        console.log(albums[i].title + " by " + albums[i].artist + " , published in " + albums[i]. yearPublished);
+    }
+}
+
+showCollection(collection);
