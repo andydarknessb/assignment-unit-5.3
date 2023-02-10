7879e2d77c1e2a305e17c13798a2032e58931163
Cory Anderson <cory.anderson6@yahoo.com>
2023-02-10 00:24:26 -0600

    add properties to function

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -11,3 +11,10 @@ function addToCollection(title, artist, yearPublished){
     collection.push(album);
     return album;
 }
+
+console.log(addToCollection("It Was Written", "Nas", 1996));
+console.log(addToCollection("I Am", "Nas", 1999));
+console.log(addToCollection("Illmatic", "Nas" , 1994));
+console.log(addToCollection("Master of Puppets", "Metallica" , 1986));
+console.log(addToCollection("It's Dark and Hell is Hot", "DMX", 1998));
+console.log(addToCollection("Flesh of My Flesh, Blood of My Blood", "DMX", 1998));
